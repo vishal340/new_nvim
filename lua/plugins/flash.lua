b90bf6bd564bf4217d5694d02572a09c07bdfaa2
vishal <vishal340@vishal-asus>
2023-08-23 21:07:44 +0530

    flash keymapping changed and incorporated into telescope

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,9 +4,9 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "n", mode = { "n", "x", "o" }, function() require("flash").jump({continue = true}) end, desc = "Flash" },
+    { "<F5>", mode = { "n", "x", "o" }, function() require("flash").jump({continue = true}) end, desc = "Flash" },
     { "*", mode = { "n", "x", "o" }, function() require("flash").jump({pattern = vim.fn.expand("<cword>"), continue = true}) end, desc = "Flash word search" },
-    { "N", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<F6>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<F2>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
--- lua/plugins/telescope-fzf.lua
+++ lua/plugins/telescope-fzf.lua
@@ -1,4 +1,6 @@
-return { 
+-- This is installed using make but it didn't work directly and i had to manually make it in the directory
+-- I suggest install it using cmake
+return {
 	'nvim-telescope/telescope-fzf-native.nvim',
 	lazy = true,
 	build = 'make',
