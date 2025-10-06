373a0e2bda278c49f8bfdb8f8d5872920f8d4c41
vishal340 <45174667+vishal340@users.noreply.github.com>
2025-10-06 20:21:04 +0530

    forgot to update the changes for telescope to main branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release && cmake --install build --prefix build",
 		},
 		"debugloop/telescope-undo.nvim",
 	},
