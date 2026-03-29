2e247eda58c444410aea8577f0df667ed2322ad3
vishal340 <45174667+vishal340@users.noreply.github.com>
2026-03-29 18:59:19 +0530

    Add render_markdown configuration with C++ and Python support

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,10 @@
+-- Render Markdown configuration for Neovim
+
+-- Enable C++ and Python code block support
+
+vim.g.vim_markdown_conceal = 0  -- Do not conceal markdown features
+
+-- C++ code block settings
+vim.g.vim_markdown_fenced_languages = {'cpp', 'python'}
+
+-- Additional settings can be added here
