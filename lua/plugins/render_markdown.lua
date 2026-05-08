f3bcffbb1a3e9ac72d50965e4c77a632b8758499
vishalaxle <vishalaxle@data-axle.com>
2026-05-08 21:13:56 +0530

    fixed the wrong location assigned for render_markdown

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -1,16 +1,23 @@
 return {
-    "render_markdown.nvim",
-    event = { "BufReadPost *.md", "BufNewFile *.md" },
-    config = function()
-        require('render_markdown').setup({
-            highlight_code_blocks = true,
-            languages = { 'cpp', 'python' },
-            auto_enable = true,
-            commands = {
-                toggle = function() require('render_markdown').toggle() end,
-                enable = function() require('render_markdown').enable() end,
-                disable = function() require('render_markdown').disable() end,
-            },
-        })
-    end,
+	"MeanderingProgrammer/render_markdown.nvim",
+	event = { "BufReadPost *.md", "BufNewFile *.md" },
+	config = function()
+		require("render_markdown").setup({
+			highlight_code_blocks = true,
+			languages = { "cpp", "python" },
+			auto_enable = true,
+			commands = {
+				toggle = function()
+					require("render_markdown").toggle()
+				end,
+				enable = function()
+					require("render_markdown").enable()
+				end,
+				disable = function()
+					require("render_markdown").disable()
+				end,
+			},
+		})
+	end,
 }
+
