9f666dff78b974909f6f2ae2f27de48c5c59ff19
vishal <vishal340@github.com>
2026-05-09 00:02:43 +0530

    added a plugin for pupyter notebook

---
+++ lua/plugins/jupyter.lua
@@ -0,0 +1,13 @@
+return {
+	"sheng-tse/jupynvim",
+	build = function(plugin)
+		local install = loadfile(plugin.dir .. "/lua/jupynvim/install.lua")()
+		install.run(plugin)
+	end,
+	config = function()
+		require("jupynvim").setup({
+			log_level = "info",
+			image_renderer = "placeholder", -- "placeholder", "kitty", or "chafa"
+		})
+	end,
+}
